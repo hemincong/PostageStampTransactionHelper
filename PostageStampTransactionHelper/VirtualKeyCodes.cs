bdea703b6d1647369e88aef8f566b5d0197d28eb
mincong.he <mincong.he@gmail.com>
2016-11-20 23:35:45 +0800

    add VirtualKeyCodes

---
+++ PostageStampTransactionHelper/VirtualKeyCodes.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable InconsistentNaming
+namespace PostageStampTransactionHelper
+{
+    public class VirtualKeyCodes
+    {
+        public const int HOTKEY_ID = 9000;
+
+        //Modifiers:
+        public const uint MOD_NONE = 0x0000; //(none)
+        public const uint MOD_ALT = 0x0001; //ALT
+        public const uint MOD_CONTROL = 0x0002; //CTRL
+        public const uint MOD_SHIFT = 0x0004; //SHIFT
+        public const uint MOD_WIN = 0x0008; //WINDOWS
+
+        public const uint VK_CAPITAL = 0x14;
+        public const uint VK_TAB = 0x09;
+        public const uint VK_RETURN = 0x0D;
+
+    }
+}
