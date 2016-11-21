6245570e34e32dd6884a624bc96df4d1a79ad39c
mincong.he <mincong.he@gmail.com>
2016-11-22 00:31:26 +0800

    分离出ViewModal

--- PostageStampTransactionHelper/ViewController/MainWindowController.cs
+++ PostageStampTransactionHelper/ViewController/MainWindowController.cs
@@ -1,70 +1,27 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using PostageStampTransactionHelper.ViewModel;
 
 namespace PostageStampTransactionHelper.ViewController
 {
-    internal class MainWindowController : INotifyPropertyChanged
+    internal class MainWindowController
     {
         public MainWindowController()
         {
-            Sale1Price = 0.01f;
-            SaleCount = 1;
-            SalePriceDelta = 0.01f;
-        }
-
-        private float _sale1Price;
-
-        public float Sale1Price
-        {
-            get { return _sale1Price; }
-            set
+            SaleItems = new List<TradingItem>
             {
-                _sale1Price = value;
-                OnPropertyChanged(nameof(Sale1Price));
-            }
-        }
-
-        private float _salePriceDelta;
-
-        public float SalePriceDelta
-        {
-            get { return _salePriceDelta; }
-            set
+                new TradingItem("X") {Price = 0.01f, Count = 1, PriceDelta = 0.01f},
+                new TradingItem("Y") {Price = 0.01f, Count = 1, PriceDelta = 0.02f},
+                new TradingItem("Z") {Price = 0.01f, Count = 1, PriceDelta = 0.03f}
+            };
+            BuyItems = new List<TradingItem>
             {
-                _salePriceDelta = value;
-                SalePriceResult = _sale1Price + _salePriceDelta;
-                OnPropertyChanged(nameof(SalePriceDelta));
-            }
+                new TradingItem("I") {Price = 0.01f, Count = 1, PriceDelta = 0.01f},
+                new TradingItem("J") {Price = 0.01f, Count = 1, PriceDelta = 0.02f},
+                new TradingItem("K") {Price = 0.01f, Count = 1, PriceDelta = 0.03f}
+            };
         }
 
-        private float _salePriceResult;
-
-        public float SalePriceResult
-        {
-            get { return _salePriceResult; }
-            set
-            {
-                _salePriceResult = value;
-                OnPropertyChanged(nameof(SalePriceResult));
-            }
-        }
-
-        private int _saleCount;
-
-        public int SaleCount
-        {
-            get { return _saleCount; }
-            set
-            {
-                _saleCount = value;
-                OnPropertyChanged(nameof(SaleCount));
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+        public List<TradingItem> BuyItems { get; }
+        public List<TradingItem> SaleItems { get; }
     }
 }
---
+++ PostageStampTransactionHelper/ViewModel/TradingItem.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+
+namespace PostageStampTransactionHelper.ViewModel
+{
+    internal class TradingItem : INotifyPropertyChanged
+    {
+        private int _count;
+        private float _price;
+        private float _priceDelta;
+
+        private float _tradingPrice;
+
+        public TradingItem(string shutCutKey)
+        {
+            ShutCutKey = shutCutKey;
+        }
+
+        public float Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                TradingPrice = _price + _priceDelta;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        public float PriceDelta
+        {
+            get { return _priceDelta; }
+            set
+            {
+                _priceDelta = value;
+                TradingPrice = _price + _priceDelta;
+                OnPropertyChanged(nameof(PriceDelta));
+            }
+        }
+
+        public float TradingPrice
+        {
+            get { return _tradingPrice; }
+            set
+            {
+                _tradingPrice = value;
+                OnPropertyChanged(nameof(TradingPrice));
+            }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+        public string ShutCutKey { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
