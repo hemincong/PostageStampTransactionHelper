1a9bd7747d30f9d6a6c10a1042ed6397a15d5000
mincong.he <mincong.he@gmail.com>
2016-11-30 00:25:17 +0800

    add virtual keys and key-name mapper

--- PostageStampTransactionHelper/VirtualKeyCodes.cs
+++ PostageStampTransactionHelper/VirtualKeyCodes.cs
@@ -1,4 +1,5 @@
-﻿// ReSharper disable InconsistentNaming
+﻿using System.Collections.Generic;
+
 namespace PostageStampTransactionHelper
 {
     public class VirtualKeyCodes
@@ -17,5 +18,32 @@ namespace PostageStampTransactionHelper
         public const uint VK_TAB = 0x09;
         public const uint VK_RETURN = 0x0D;
 
+        public const uint A_KEY = 0x41;
+        public const uint B_KEY = 0x42;
+        public const uint I_KEY = 0x49;
+        public const uint J_KEY = 0x4A;
+        public const uint K_KEY = 0x4B;
+        public const uint X_KEY = 0x58;
+        public const uint Y_KEY = 0x59;
+        public const uint Z_KEY = 0x5A;
+
+        static private Dictionary<uint, string> key_name_mapper = new Dictionary<uint, string>
+        {
+            { A_KEY, "A" },
+            { B_KEY, "B" },
+            { I_KEY, "I" },
+            { J_KEY, "J" },
+            { K_KEY, "K" },
+            { X_KEY, "X" },
+            { Y_KEY, "Y" },
+            { Z_KEY, "Z" },
+        };
+
+        static public string GetKeyName(uint virtualKey)
+        {
+            string name;
+            key_name_mapper.TryGetValue(virtualKey, out name);
+            return name;
+        }
     }
 }
