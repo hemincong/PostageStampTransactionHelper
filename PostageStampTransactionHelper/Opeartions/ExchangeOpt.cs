42f065522e7c23a0f5499ed8bbf19ed2da00dab8
mincong.he <hemincong@gmail.com>
2016-12-15 23:53:08 +0800

    + 查找按钮的动作 以及fix position

--- PostageStampTransactionHelper/Opeartions/ExchangeOpt.cs
+++ PostageStampTransactionHelper/Opeartions/ExchangeOpt.cs
@@ -14,9 +14,13 @@ namespace PostageStampTransactionHelper.Opeartions
         // Item4 : 区域底部相对Window底部位置
         protected abstract Tuple<int, int, int, int> FindWordOffest { get; }
         protected abstract Tuple<int, int, int, int> FindInputOffest { get; }
+        protected abstract Tuple<int, int, int, int> ButtonOffest { get; }
 
         protected abstract string BaseWord();
         protected abstract string InputBaseWord();
+        protected abstract string ButtonName();
+        protected abstract int StateKey();
+
 
         public void ExchangeOpt()
         {
@@ -34,19 +38,24 @@ namespace PostageStampTransactionHelper.Opeartions
                 return;
             }
 
-            // ReSharper disable once InconsistentNaming
-            dm.KeyPress((int) VirtualKeyCodes.VF_F12);
+            Thread.Sleep(300);
+            dm.KeyPress(StateKey());
 
             dm.SetDict(0, "words.txt");
 
             object sale1PosX, sale1PosY;
             var o = FindWordOffest;
             dm.FindStr((int) x1 + o.Item1, (int) y2 + o.Item2, (int) x1 + o.Item3, (int) y2 + o.Item4, BaseWord(),
-                "000000-000000", 1.0,
+                "008000-008000|ff0000-ff0000|000000-000000|0000ff-0000ff", 1.0,
                 out sale1PosX, out sale1PosY);
+            if ((int)sale1PosX < 0 || (int)sale1PosY < 0)
+            {
+                MessageUtils.ShowError("找不到字段");
+                return;
+            }
 
             var ocrRet = dm.Ocr((int) sale1PosX + 30, (int) sale1PosY, (int) sale1PosX + 90, (int) sale1PosY + 20,
-                "008000-008000|ff0000-ff0000|000000-000000", 1.0);
+                "008000-008000|ff0000-ff0000|000000-000000|0000ff-0000ff", 1.0);
 
             float currPrice;
             float.TryParse(ocrRet, out currPrice);
@@ -54,14 +63,37 @@ namespace PostageStampTransactionHelper.Opeartions
             var s = currPrice.ToString("0.00");
             object salePricePosX, salePricePosY;
 
-            var o2 = FindWordOffest;
+            var o2 = FindInputOffest;
             dm.FindStr((int) x1 + o2.Item1, (int) y2 + o2.Item2, 
-                (int) x1 + o2.Item3, (int) y2 + o2.Item4, InputBaseWord(), "0000ff-0000ff", 1.0,
+                (int) x1 + o2.Item3, (int) y2 + o2.Item4, InputBaseWord(),
+                "008000-008000|ff0000-ff0000|000000-000000|0000ff-0000ff", 1.0,
                 out salePricePosX, out salePricePosY);
-            dm.MoveTo((int) salePricePosX + 130, (int) salePricePosY + 10);
+
+            object mousePosX, mousePosY;
+            dm.GetCursorPos(out mousePosX, out mousePosY);
+
+            dm.MoveTo((int) salePricePosX + 90, (int) salePricePosY + 10);
             dm.LeftClick();
             foreach (var c in s.ToCharArray())
                 dm.KeyPressChar(c.ToString());
+
+            var o3 = ButtonOffest;
+            object buttonPosX, buttonPosY;
+            //dm.FindStr(251, 921, 282, 938, ButtonName(),
+            dm.FindStr((int) x1 + o3.Item1, (int) y2 + o3.Item2, 
+                (int) x1 + o3.Item3, (int) y2 + o3.Item4, ButtonName(),
+                "008000-008000|ff0000-ff0000|000000-000000|0000ff-0000ff", 1.0,
+                out buttonPosX, out buttonPosY);
+
+            if ((int)buttonPosX < 0 || (int)buttonPosY < 0)
+            {
+                MessageUtils.ShowError("找不到目标");
+                return;
+            }
+            dm.MoveTo((int)buttonPosX, (int) buttonPosY + 10);
+            dm.LeftClick();
+
+            dm.MoveTo((int) mousePosX, (int) mousePosY);
         }
     }
 }
